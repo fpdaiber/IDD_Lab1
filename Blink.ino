ffbc00a4d44a17aea466e8eaed763e3f338d1fc2
fpdaiber <37711750+fpdaiber@users.noreply.github.com>
2019-09-09 23:30:53 -0400

    Add files via upload

--- Blink.ino
+++ Blink.ino
@@ -22,24 +22,26 @@
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+int led = 9;           // the PWM pin the LED is attached to
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(800);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(800);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
